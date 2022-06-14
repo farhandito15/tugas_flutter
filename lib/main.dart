1b40e3874f7655f15e1f6affa2c64bd6786cd7c6
farhandito15 <farhandito15@gmail.com>
2022-06-14 11:43:52 +0700

    commit state management

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Text(
-                'Counter App menggunakan State Management Flutter',
+                'Counter App menggunakan State Management Flutter Farhan',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 16),
               ),
