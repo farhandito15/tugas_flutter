d1cd741632cd77532cf671fe6c960bba65d5c8f3
farhandito15 <farhandito15@gmail.com>
2022-06-14 16:56:54 +0700

    commit state management getx

--- lib/main2.dart
+++ lib/main2.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MaterialApp',
+      title: 'MaterialApps',
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
